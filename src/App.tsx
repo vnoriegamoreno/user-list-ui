dbaa166b8e696f37d9e1f86db66723a69bb98d4f
Vicente Spencer Noriega Moreno <vicente.noriega.sc@gmail.com>
2024-10-24 23:46:43 -0600

    task-4 - Install and Setup React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,13 @@
-import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { setUsers } from 'store/slices/userSlice';
-import fakeAPIUserListRes from 'mock/user-list.mock.json';
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Main from 'pages/Main/Main';
 
-const App: React.FC = () => {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    const loadUserList = async (): Promise<void> => {
-      const response = await fetch('http://localhost:8080/api/user');
-      try {
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        const userList = await response.json();
-        dispatch(setUsers(userList.data || []));
-      } catch (err) {
-        console.error('Error fetching user data:', err);
-      }
-    };
-    loadUserList();
-  }, [dispatch]);
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-};
+const App: React.FC = () => (
+  <Router>
+    <Routes>
+      <Route path="/" element={<Main />} />
+    </Routes>
+  </Router>
+);
 
 export default App;
