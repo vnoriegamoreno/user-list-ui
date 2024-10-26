607a067e5243a7b6102ba59b3461eaa34a81be65
Vicente Spencer Noriega Moreno <vicente.noriega.sc@gmail.com>
2024-10-25 20:44:46 -0600

    task-5 - Implement Profile route and show all details of the user selected

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Main from 'pages/Main/Main';
+import Profile from 'pages/Profile/Profile';
 
 const App: React.FC = () => (
   <Router>
     <Routes>
       <Route path="/" element={<Main />} />
+      <Route path="/profile/:id" element={<Profile />} />
     </Routes>
   </Router>
 );
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,32 @@
+import { useNavigate } from 'react-router-dom';
+import { ReactSVG } from 'react-svg';
+import leftArrow from 'assets/img/left-arrow.svg';
+
 interface Props {
   title: string;
+  back?: boolean;
 }
 
-const Header = ({ title }: Props) => (
-  <header className="Header" data-testid="Header">
-    <h1 className="Title">{title}</h1>
-  </header>
-);
+const Header = ({ title, back = false }: Props) => {
+  const navigate = useNavigate();
+
+  const goToMainPage = (): void => navigate('/');
+
+  return (
+    <header className="Header" data-testid="Header">
+      <h1 className="Title">
+        {back && (
+          <ReactSVG
+            className="LeftArrow"
+            data-testid="LeftArrow"
+            onClick={goToMainPage}
+            src={leftArrow}
+          />
+        )}
+        {title}
+      </h1>
+    </header>
+  );
+};
 
 export default Header;
