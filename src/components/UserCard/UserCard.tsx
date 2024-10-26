df341e26661ee6d9fb31928f7079d1ed7b33dead
Vicente Spencer Noriega Moreno <vicente.noriega.sc@gmail.com>
2024-10-25 18:35:22 -0600

    task-4 - Create User Card Component

--- src/components/UserCard/UserCard.tsx
+++ src/components/UserCard/UserCard.tsx
@@ -1,13 +1,15 @@
 import { User } from 'types/userTypes';
+import { ReactSVG } from 'react-svg';
+import avatar from 'assets/img/avatar.svg';
 
 const UserCard = ({ id, name }: User) => (
-  <ul>
-    <li>ID: {id}</li>
-    <li>Name: {name}</li>
-    <li>
-      <button>View details</button>
-    </li>
-  </ul>
+  <div className="UserCard" data-testid="UserCard">
+    <ReactSVG data-testid="AvatarSVG" src={avatar} className="Avatar" />
+    <h3 className="UserName">{name}</h3>
+    <button className="ViewUserDetails" id={id}>
+      View details
+    </button>
+  </div>
 );
 
 export default UserCard;
